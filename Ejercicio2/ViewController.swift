0e0c37bbcc1d4a4d650c95a40808fd605c0e9511
Edgar Tobón Sosa <etbn@MacBook-Pro-de-Edgar.local>
2022-11-27 15:51:51 -0600

    Ejercicio 2 Audio Player

---
+++ Ejercicio2/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Ejercicio2
+//
+//  Created by DISMOV on 17/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet var btnPlay: UIButton!
+    @IBOutlet var btnStop: UIButton!
+    
+    @IBOutlet var sliderDuration: UISlider!
+    @IBOutlet var sliderVolume: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    @IBAction func play(_ sender: UIButton) {
+    }
+    
+    @IBAction func stop(_ sender: UIButton) {
+    }
+    
+    @IBAction func song(_ sender: UISlider) {
+    }
+    
+    @IBAction func volume(_ sender: UISlider) {
+    }
+}
+
